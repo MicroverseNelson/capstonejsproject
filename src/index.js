4b6a5b7db3de0dfe7dd4258af1751bccfe3c5f0d
MicroverseNelson <poetpenton@gmail.com>
2022-08-06 08:37:18 +0300

    linter fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import _ from 'lodash';
+/** import _ from 'lodash';
 import './style.css';
 import scifi from './assets/scifi.jpg';
 import space3 from './assets/space3.jpeg';
 import space4 from './assets/space4.jpeg';
 import space5 from './assets/space5.jpeg';
-
+**/
 
